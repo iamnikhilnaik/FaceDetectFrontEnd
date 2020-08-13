53bd7778d8f6932fa865aa47fdabff9818cb7ef0
iamnikhilnaik <nnaikme@gmail.com>
2020-08-13 21:49:49 +0530

    adding front end project

---
+++ src/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rank = ({ name, }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, I Detect Face`}
+      </div>
+      
+    </div>
+  );
+}
+
+export default Rank;
